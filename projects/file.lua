27af40347e8963d4792b004ed2a9bb010605a225
Chase Eller <kj4lxc@users.noreply.github.com>
2024-05-30 17:23:25 -0400

    test

---
+++ projects/file.lua
@@ -0,0 +1,39 @@
+
+
+
+--File Manipution
+if not fs.exists('/data') then
+  fs.makeDir('/data')
+end
+
+function write(data, name, folder)
+  if folder then 
+    local f = fs.open('/'..folder..'/'..name, 'w')
+    f.write(textutils.serialize(data))
+    f.close()
+  else
+    local f = fs.open('/data/'..name, 'w')
+    f.write(textutils.serialize(data))
+    f.close()
+  end
+  
+end
+
+function read(name, folder)
+  local data = nil
+  if folder then
+    if fs.exists('/'..folder..'/'..name) then
+      local f = fs.open('/'..folder..'/'..name, 'r')
+      data = textutils.unserialize(f.readAll())
+      f.close()
+    end
+  else
+    if fs.exists('/data/'..name) then
+      local f = fs.open('/data/'..name, 'r')
+      data = textutils.unserialize(f.readAll())
+      f.close()
+    end
+  end
+
+  return data
+end
